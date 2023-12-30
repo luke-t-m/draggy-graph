82ed5999d473da8b3be63564179ea32661a0d4fb
luke <luketmckay@outlook.com>
2023-12-30 21:50:53 +0000

    can now drag nodes around. we're getting close!

--- graph_canvas.py
+++ graph_canvas.py
@@ -6,6 +6,7 @@ class GraphCanvas(UberCanvas):
         UberCanvas.__init__(self, root)
 
         self.tool = "move"
+        self.dragging = None
 
         self.node_radius = 0.25
 
@@ -24,11 +25,26 @@ class GraphCanvas(UberCanvas):
             tags=tags
         )
     
+    def move_circle(self, id, x, y):
+        current_coords = self.canvas.coords(id)
+        radius = (current_coords[2] - current_coords[0]) / 2
+        self.canvas.coords(id, x - radius, y - radius, x + radius, y + radius)
+
     def place_node(self, event):
         x = self.canvas.canvasx(event.x)
         y = self.canvas.canvasy(event.y)
         node_id = self.draw_circle(x, y, self.node_radius, "red", tags=("node", "draggable"))
         self.node_ids.append(node_id)
+
+    def find_draggable(self, event):
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        mod = 0.01
+        under_mouse = self.canvas.find_overlapping(x - mod, y - mod, x + mod, y + mod)
+        for id in under_mouse:
+            if "draggable" in self.canvas.gettags(id):
+                return id
+    
     
     # Functions that yearn to be bound.
     
@@ -38,8 +54,7 @@ class GraphCanvas(UberCanvas):
                 self.start_canvas_drag(event)
             case "place":
                 self.place_node(event)
-            case "drag_node":
-                return
+            case "drag":
                 self.dragging = self.find_draggable(event)
 
 
@@ -59,8 +74,7 @@ class GraphCanvas(UberCanvas):
             case "place":
                 pass
             case "drag":
-                return
-                self.move_circle(self.dragging, self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))
-                self.draw_midline()
+                if self.dragging != None:
+                    self.move_circle(self.dragging, self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))
 
 
